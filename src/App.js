f29039f227a8b4cf1a0fba3b60b73c116af1ada1
Cihan Yaralı <109430346+Cihanyarali@users.noreply.github.com>
2023-04-05 13:15:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+import TasksContext from "./context/task";
+import { useEffect, useContext } from "react";
+
+function App() {
+  const { fetchTasks } = useContext(TasksContext);
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div className="App">
+      <TaskCreate />
+      <h1>Görevler</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
